b72ec60353888d0eb23d7ab8614ac9b1f42e25f4
DeepakDinkar <ddeepu794@gmail.com>
2023-03-07 09:06:46 +0530

    Moved header to left

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import Navbar from "react-bootstrap/Navbar";
 const Header = () => {
   return (
     <Navbar fixed="top" className="app-bg-primary">
-      <Container>
+      <Container fluid>
         <Navbar.Brand className="fw-bold font-tilt-prism">
           BLOCKBUSTER
         </Navbar.Brand>
